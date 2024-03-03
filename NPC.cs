da577bad8af4fe29504f06b97d4ac983c0efe201
FIXINIT3 <km1308283@gmail.com>
2024-03-03 18:29:38 -0500

    Diden't do much
    got demotivated and only added a actual area the setting lead too and a slider that does nothing,

---
+++ NPC.cs
@@ -0,0 +1,9 @@
+using Godot;
+using System;
+
+public partial class NPC : CharacterBody2D
+{
+//	void update{
+//	sprite.Play("default");
+//	}
+}
--- Text.gd
+++ Text.gd
@@ -4,7 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Area2D.body_entered.connect(self._play_animation)
-	
+
 func _play_animation(_body):
 	$Area2D.body_entered.disconnect(self._play_animation)
 	$AnimationPlayer.play('text')
