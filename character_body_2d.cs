186c232804546a9fac391d938fadfccb81c6cbe3
Lirkz <davidsaiontz2008@gmail.com>
2024-03-04 16:03:31 -0500

    double jump

--- character_body_2d.cs
+++ character_body_2d.cs
@@ -7,6 +7,7 @@ public partial class character_body_2d : CharacterBody2D
 	public const float JumpVelocity = -550.0f;
 	private AnimatedSprite2D sprite;
 	public bool isLeft;
+	public bool doubleJump; //Maybe replace with some other kind of midair mobility in the future
 	
 	public override void _Ready()
 	{
@@ -18,28 +19,50 @@ public partial class character_body_2d : CharacterBody2D
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
-
+		
 		// Add the gravity.
 		if (!IsOnFloor())
 		{
 			velocity.Y += gravity * (float)delta;
-			if (velocity.Y > 0)
+		}
+		else 
+		{
+			doubleJump = true;
+		}
+		// animation
+		if (velocity.Y > 0)
 		{
 			sprite.Play("fall");
 		}
 		else if (velocity.Y < 0)
 		{
-			sprite.Play("jump");
+			if  (doubleJump == true)
+			{
+				sprite.Play("jump");
+			}
+			else 
+			{
+				sprite.Play("double jump");
+			}
 		}
-			}
-			else if (!Input.IsKeyPressed(Key.A) && !Input.IsKeyPressed(Key.D)){
-				sprite.Play("default");
-			} else {
-				sprite.Play("run");
-			}
+		else if (!Input.IsKeyPressed(Key.A) && !Input.IsKeyPressed(Key.D)){
+			sprite.Play("default");
+		} 
+		else 
+		{
+			sprite.Play("run");
+		}
 		// Handle Jump.
 		if (Input.IsActionJustPressed("jump") && IsOnFloor())
+		{
 			velocity.Y = JumpVelocity;
+			sprite.Play("jump");
+		}
+		else if (Input.IsActionJustPressed("jump") && doubleJump == true)
+		{
+			velocity.Y = JumpVelocity;
+			doubleJump = false;
+		}
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
